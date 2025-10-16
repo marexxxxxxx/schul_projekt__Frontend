6522bc58ad40db21196061aa2272b3baeba798e5
Ramsey 007 <maxx.goetz@gmail.com>
2025-10-16 13:57:01 +0000

    Entferne mir die Makierungen für Berge. Die Dreiecke auf der Karte

--- src/components/map-component.tsx
+++ src/components/map-component.tsx
@@ -23,8 +23,8 @@ export default function MapComponent({ position, zoom, markerPosition, markerPop
     if (typeof window !== 'undefined' && mapContainerRef.current && !mapRef.current) {
       mapRef.current = L.map(mapContainerRef.current, { attributionControl: false, zoomControl: false }).setView(position, zoom);
       
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
       }).addTo(mapRef.current);
     }
   }, [position, zoom]);
